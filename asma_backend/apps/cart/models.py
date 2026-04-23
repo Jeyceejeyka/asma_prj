d476533531ac26e82ef5996ea2820b3817b60e72
jeycee <jeyceejeyka635@gmail.com>
2026-04-24 00:02:56 +0300

    adds models creations for cart with relationship plus intialized empty files for urls and serializers

--- asma_backend/apps/cart/models.py
+++ asma_backend/apps/cart/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField('accounts.User', on_delete=models.CASCADE, related_name='cart')
+    # user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='carts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    products = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        '''Ensures that a product can only be added once to a cart. If the same product is added again, the quantity should be updated instead of creating a new CartItem.'''
+        unique_together = ('cart', 'products')
+        
+    def __str__(self):
+        return f'{self.products.name} x {self.quantity}'
